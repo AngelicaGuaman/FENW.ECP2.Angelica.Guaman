517fa1b6af37214eb8cbf4ca67cf9c1c257705ed
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-11-28 23:46:02 +0100

    updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InstalacionesComponent } from './components/instalaciones/instalaciones.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent,
+    FooterComponent,
+    InstalacionesComponent,
+    ReservaComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
