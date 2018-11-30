76f0327d475f77e5aa7b091df7e57ab40536077f
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-11-30 15:55:31 +0100

    refactoring menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { InstalacionesComponent } from './components/instalaciones/instalaciones.component';
 import { ReservaComponent } from './components/reserva/reserva.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    HeaderComponent,
     FooterComponent,
     InstalacionesComponent,
     ReservaComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
