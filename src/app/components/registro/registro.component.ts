61352e51a2f374e925868036143c5e80dc8174a7
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-12-06 00:02:32 +0100

    added signInForm

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../shared/services/auth.service';
+import {User} from '../../shared/models/user.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  public user: User;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+  }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([Validators.required])),
+      email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl('', Validators.compose([Validators.required])),
+      repeatPassword: new FormControl('', Validators.compose([Validators.required])),
+      birthdate: new FormControl(0)
+    });
+  }
+
+  onSignIn(form: FormGroup) {
+    const birthdate = new Date(form.value.birthdate);
+
+    this.user = new User(form.value.username, form.value.email, form.value.password, form.value.repeatPassword, birthdate.getTime());
+
+    this.authService.registerUser(this.user).subscribe(
+      response => {
+        sessionStorage.setItem('accessToken', response.headers.get('Authorization'));
+        sessionStorage.setItem('currentUser', this.form.value.username);
+
+      });
+  }
+
+  checkUser(username: string) {
+    this.authService.checkUser(username).subscribe(
+      response => {
+
+      });
   }
 
 }
