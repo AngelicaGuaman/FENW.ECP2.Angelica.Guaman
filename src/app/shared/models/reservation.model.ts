c953ccc887f0750b6ded7f1fe203d3fb507e6847
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-12-06 14:28:24 +0100

    added reservation model and service

---
+++ src/app/shared/models/reservation.model.ts
@@ -0,0 +1,10 @@
+export class Reservation {
+  constructor(rsvId: number, courtId: number, rsvdateTime: number, rsvday: string, rsvtime: string) {
+  }
+
+  rsvId: number;
+  courtId: number;
+  rsvdateTime: number;
+  rsvday: string;
+  rsvtime: string;
+}
---
+++ src/app/shared/services/reservations.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Reservation} from '../models/reservation.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationsService {
+
+  private baseurl = 'http://fenw.etsisi.upm.es:5555/reservations';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllReservationsByLoggedUser() {
+    return this.http.get<Reservation[]>(this.baseurl, {
+      headers: new HttpHeaders({'Authorization': sessionStorage.getItem('accessToken')})
+    });
+  }
+
+  getAllReservationsByAllUsersInASpecificDate(dateNumbre: number) {
+    return this.http.get<Reservation[]>(this.baseurl + '/' + dateNumbre, {
+      headers: new HttpHeaders({'Authorization': sessionStorage.getItem('accessToken')})
+    });
+  }
+
+  doReservation(courtId: number, rsvdateTime: number) {
+    return this.http.post(this.baseurl, {courtId: courtId, rsvdateTime: rsvdateTime}, {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'Authorization': sessionStorage.getItem('accessToken')}),
+      observe: 'response'
+    });
+  }
+}
